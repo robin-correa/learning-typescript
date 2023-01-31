01132b250f584cb2b5c4042117a868d00ae5d450
Robin Correa <robin.correa21@gmail.com>
2023-01-31 22:32:22 +0800

    Getting started. Learn arrays, tuples and enums

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World, Robin!')
+// [Arrays]
+
+// To initialize empty array, must put the data type
+let numbers: number[] = [];
+
+// [Tuples] - useful with only have 2 values / key-value pair
+
+let user: [number, string] = [1, 'Robin']
+
+// [Enums]
+const enum Size { Small = 1, Medium, Large }
+let mySize: Size = Size.Medium;
+console.log(mySize)

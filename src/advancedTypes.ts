da73ec91a2f01bdcee5e5ccce57830948fd9bd49
Robin Correa <robin.correa21@gmail.com>
2023-02-03 18:57:01 +0800

    Learn Union Types in typescript.

--- src/advancedTypes.ts
+++ src/advancedTypes.ts
@@ -13,3 +13,16 @@ let employee1: Employee = {
         console.log(date);
     }
 };
+
+// [Union Types]
+function kgToLbs(weight: number | string): number {
+    // Narrowing
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    } else {
+        return parseInt(weight) * 2.2;
+    }
+}
+
+kgToLbs(10);
+kgToLbs('10kg');

83ad15eb0a404c3869dfa257d7e3283e1214d222
Robin Correa <robin.correa21@gmail.com>
2023-02-03 18:02:51 +0800

    Learning functions in typescript.

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,15 @@ let user: [number, string] = [1, 'Robin']
 const enum Size { Small = 1, Medium, Large }
 let mySize: Size = Size.Medium;
 console.log(mySize)
+
+// [Functions]
+// Set default values, properly annotate your functions, no implicit returns
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+
+    return income * 1.4
+}
+
+calculateTax(10_000);

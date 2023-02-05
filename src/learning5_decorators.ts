3c9e82b2c8cf69ebb2cd24c0d662b77418cadedd
Robin Correa <robin.correa21@gmail.com>
2023-02-05 14:22:37 +0800

    Learning Class Decorators in typescript.

---
+++ src/learning5_decorators.ts
@@ -0,0 +1,28 @@
+/*
+class Component {
+    insertInDOM() {
+
+    }
+}
+
+class ProfileComponent extends Component{
+
+}
+
+*/
+
+// [Class Decorators]
+function Component(constructor: Function) {
+    console.log('Component decorator called');
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+        console.log('Inserting the component in the DOM');
+    }
+}
+
+@Component
+class ProfileComponent {
+
+}
+
+

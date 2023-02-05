6cb7be151d0372226efb7a3c20b9c559de8424f1
Robin Correa <robin.correa21@gmail.com>
2023-02-05 09:51:50 +0800

    Learning Extending Generic classes.

--- src/learning4_generics.ts
+++ src/learning4_generics.ts
@@ -95,3 +95,48 @@ function echo<T extends PersonClass>(value: T): T {
 }
 
 echo(new CustomerClass);
+
+// [Extending Generic Classes]
+
+interface Product {
+    name: string;
+    price: number;
+}
+
+class Store<T> {
+    protected _objects: T[] = [];
+
+    add(obj: T): void {
+        this._objects.push(obj);
+    }
+}
+
+let store1 = new Store<Product>();
+// store1.objects = [];
+
+class CompressibleStore<T> extends Store<T> {
+    compress() { }
+
+}
+
+let store2 = new CompressibleStore<Product>();
+store2.compress()
+
+// Restrict the generic type parameter
+class SearchableStore<T extends { name: string }> extends Store<T> {
+    find(name: string): T | undefined {
+        return this._objects.find(obj => obj.name === name);
+    }
+}
+
+class ProductStore extends Store<Product> {
+    filterByCategory(category: string): Product[] {
+        return [];
+    }
+}
+
+// Key takeaway: When extending a generic class:
+// Fix the generic type parameter
+// We can restrict it
+// Simply pass it on the child class
+

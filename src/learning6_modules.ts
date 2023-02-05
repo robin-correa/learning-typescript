c72e0e422a3f153995d21373f538cab87097d676
Robin Correa <robin.correa21@gmail.com>
2023-02-05 15:41:28 +0800

    Learning Wildcard imports in typescript.

--- src/learning6_modules.ts
+++ src/learning6_modules.ts
@@ -14,3 +14,9 @@ let store = new Store();
 console.log(store);
 console.log(Format.Raw) // 0 (enum)
 console.log(Format.Compressed) // 1 (enum)
+
+// [Wildcard imports]
+import * as Shapes from './shapes';
+
+let circle2 = new Shapes.Circle(2);
+console.log(circle2);

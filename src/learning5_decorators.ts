e26a68d96e09ca4a0419122794bcb3936d7d2da4
Robin Correa <robin.correa21@gmail.com>
2023-02-05 14:29:56 +0800

    Learning parametized decorators in typescript.

--- src/learning5_decorators.ts
+++ src/learning5_decorators.ts
@@ -21,8 +21,26 @@ function Component(constructor: Function) {
 }
 
 @Component
-class ProfileComponent {
+class ProfileComponent1 { }
 
+
+// [Parametized Decorators]
+
+type ComponentOptions = {
+    selector: string
 }
 
+// Decorator Factory - will act as factory for creating decorator
+function ParentComponent(value: ComponentOptions) {
+    return (constructor: Function) => {
+        console.log('Component decorator called');
+        constructor.prototype.options = value;
+        constructor.prototype.uniqueId = Date.now();
+        constructor.prototype.insertInDOM = () => {
+            console.log('Inserting the component in the DOM');
+        }
+    }
+}
 
+@ParentComponent({ selector: '#my-profile' })
+class ProfileComponent2 { }

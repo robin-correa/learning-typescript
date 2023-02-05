fe7dfe70db30e0003daa860cd2f4317c3e23c8af
Robin Correa <robin.correa21@gmail.com>
2023-02-05 10:19:59 +0800

    Learning Type Mapping in typescript.

--- src/learning4_generics.ts
+++ src/learning4_generics.ts
@@ -166,3 +166,56 @@ store3.find('name', 'a');
 store3.find('price', 1);
 // store3.find('nonExistingProperty', 1); // Compilation error
 
+// [Type Mapping]
+
+interface Product2 {
+    name: string;
+    price: number;
+}
+
+type ReadOnlyProduct = {
+    // Index signature
+    // keyof
+
+    readonly [K in keyof Product2]: Product[K]
+}
+
+let product1: ReadOnlyProduct = {
+    name: 'a',
+    price: 1
+}
+
+// product.name = 'b'; // Compilation error / read only
+
+type ReadOnly<T> = {
+    // Index signature
+    // keyof
+
+    readonly [K in keyof T]: T[K]
+}
+
+type Optional<T> = {
+    [K in keyof T]?: T[K]
+}
+
+type Nullable<T> = {
+    [K in keyof T]?: T[K] | null
+}
+
+let product2: ReadOnly<Product2> = {
+    name: 'a',
+    price: 1
+}
+
+/* 
+    Summary
+
+    - Generics allow us to create reusable classes, interfaces and functions.
+    - A generic type has one or more generic type parameters specified in angle brackets.
+    - When using generic types, we should supply arguments for generic type parameters or let the compiler infer them (if possible).
+    - We can constrain generic type arguments by using the extends keyword after generic type parameters. 
+    - When extending generic classes, we have three options: can pass on generic type parameters, so the derived classes will have the same generic type parameters. Alternatively, we can restrict or fix them. 
+    - The [keyof] operator produces a union of the keys of the given object.
+    - Using type mapping we can create new types based off of existing types. For example, we can create a new type with all the properties of another type where these properties are readonly, optional, etc.
+    - TypeScript comes with several utility types that perform type mapping for us. Examples are: Partial<T>, Required<T>, Readonly<T>, etc.
+*/
